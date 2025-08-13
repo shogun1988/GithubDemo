2bdd26956f9371b76e486e106b0da2a1937ed873
nokah vandenhoven <vandenhoven_nokah@hotmail.com>
2025-08-13 09:26:04 +0200

    GithubDemo

---
+++ index.ts
@@ -0,0 +1,21 @@
+function findDuplicateCharacters(str: string): string[] {
+    const charCount: { [key: string]: number } = {};
+    const duplicates: string[] = [];
+
+    for (const char of str) {
+        if (charCount[char]) {
+            charCount[char]++;
+        } else {
+            charCount[char] = 1;
+        }
+    }
+
+    for (const char in charCount) {
+        if (charCount[char] > 1) {
+            duplicates.push(char);
+        }
+    }
+
+    return duplicates;
+}
+
