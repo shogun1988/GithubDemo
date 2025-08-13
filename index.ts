206e58cb0e94ced72a231561477c27e71eedeb01
nokah vandenhoven <vandenhoven_nokah@hotmail.com>
2025-08-13 09:49:03 +0200

    demoTest

--- index.ts
+++ index.ts
@@ -21,25 +21,3 @@ function findDuplicateCharacters(str: string): string[] {
 
 // this a sample file for testing purpose
 
-
-function findDuplicateCharacters(str: string): string[] {
-    const charCount: { [key: string]: number } = {};
-    const duplicates: string[] = [];
-
-    for (const char of str) {
-        if (charCount[char]) {
-            charCount[char]++;
-        } else {
-            charCount[char] = 1;
-        }
-    }
-
-    for (const char in charCount) {
-        if (charCount[char] > 1) {
-            duplicates.push(char);
-        }
-    }
-
-    return duplicates;
-}
-
